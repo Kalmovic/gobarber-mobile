9e1d12faac5db1f9a3e083ae5a32007d4b2f57fd
Joseph <joseph.kal@outlook.com>
2019-07-27 08:00:45 -0300

    added styled register page

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { Text } from 'react-native';
-import Background from '../../components/Background';
-// import { Container } from './styles';
+import { Image } from 'react-native';
 
-export default function SignUp() {
+import logo from '../../assets/logo.png';
+import Background from '../../components/Background';
+
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
+export default function SignUp({ navigation }) {
   return (
     <Background>
-      <Text>SignUp</Text>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Full name"
+          />
+          <FormInput
+            icon="mail-outline"
+            keyboardTyoe="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="E-mail"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Password"
+          />
+          <SubmitButton onPress={() => {}}>Log in</SubmitButton>
+        </Form>
+        <SignLink onPress={() => navigation.navigate('SignIn')}>
+          <SignLinkText>Already registered</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
